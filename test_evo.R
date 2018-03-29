e521e337e43cb2129cf67f88d9cbd6915f7c0ff5
achimrumberger@icloud.com <achimrumberger@icloud.com>
2018-03-29 11:03:53 +0200

    new test with multple runs

--- test_evo.R
+++ test_evo.R
@@ -1,5 +1,8 @@
 library(testthat)
+library(R6)
+
 source("EvoOrganism.R")
+
 context('evoOrg function correctly')
 evoOrg1 <- EvoOrganism$new(tempOpt = 20, nutrientMin = 20, orgCount = 1)
 evoOrg1$set_envNutrient(20)
@@ -8,7 +11,7 @@ evoOrg1$determineGrowthrate()
 evoOrg1$determineOrgCount()
 test_that('org count correct', {
   expect_equal(evoOrg1$orgCount, 2)
-  expect_equal(evoOrg1$growthrate, 1)
+  expect_equal(evoOrg1$growthrate, 2)
 })
 
 
@@ -22,4 +25,16 @@ test_that('org count correct', {
   expect_equal(evoOrg2$growthrate, 1.5)
 })
 
+#multiple runs
+evoOrg3 <- EvoOrganism$new(tempOpt = 20, nutrientMin = 20, orgCount = 1)
+evoOrg3$set_envNutrient(20)
+evoOrg3$set_envTemperature(20)
+evoOrg3$determineGrowthrate()
+evoOrg3$determineOrgCount()
+evoOrg3$determineGrowthrate()
+evoOrg3$determineOrgCount()
+test_that('org count correct with multiple runs', {
+  expect_equal(evoOrg3$orgCount, 4)
+  expect_equal(evoOrg3$growthrate, 2)
+})
 
